cd7111e91aff58b9dcdf2c2cf39f7b9ae85fa532
Lakshanikodikara <61499073+Lakshanikodikara@users.noreply.github.com>
2020-04-02 19:04:27 +0530

    done tute 5

---
+++ app/src/main/java/com/example/testapplicationfirebase/student.java
@@ -0,0 +1,44 @@
+package com.example.testapplicationfirebase;
+
+public class student {
+    private String ID;
+    private String name;
+    private String address;
+    private Integer conNo;
+
+    public student() {
+
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getConNo() {
+        return conNo;
+    }
+
+    public void setConNo(Integer conNo) {
+        this.conNo = conNo;
+    }
+}
